a2ff1d042b27abafbefadf73d66eb675dcb74957
UpendraPandit <126135709+UpendraPandit@users.noreply.github.com>
2023-12-06 18:46:46 +0100

    Update db.js

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2')
 const mysqlConnection = mysql.createConnection({
     // mysql://doadmin:AVNS_Tbd0LAzTqYn4_sZaLk-@db-mysql-nyc1-57626-do-user-14369736-0.b.db.ondigitalocean.com:25060/defaultdb?ssl-mode=REQUIRED
     
-        host: 'db-mysql-nyc1-57626-do-user-14369736-0.b.db.ondigitalocean.com',
+        host: 'db-mysql-nyc3-50115-do-user-15325170-0.c.db.ondigitalocean.com',
         user: 'doadmin',
-        password: 'AVNS_Tbd0LAzTqYn4_sZaLk',
+        password: 'AVNS_XRgsgECTXd3joI0LVhM',
         database:'defaultdb',
         port:25060,
 });
